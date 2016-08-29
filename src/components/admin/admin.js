556b7fb66c98ea1e18260b68fa4ebd275e2a43cb
blackendstudios <jeroflat@gmail.com>
2016-08-28 23:20:34 -0500

    feat(adminDashboard): lazyload <admin-dashboard> after user is logged in

--- src/components/admin/admin.js
+++ src/components/admin/admin.js
@@ -2,11 +2,7 @@ import angular from 'angular';
 import adminComponent from './admin.component';
 import AdminService from './admin.service';
 
-import adminDashboard from './admin-dashboard/admin-dashboard';
-
-let adminModule = angular.module('app.admin', [
-    adminDashboard.name
-  ])
+let adminModule = angular.module('app.admin', [])
   .component('admin', adminComponent)
   .service('AdminService', AdminService);
 
