8851f899563f0d4ac4cc0f42a5406026092f2a14
blackendstudios <jeroflat@gmail.com>
2016-08-28 20:51:09 -0500

    feat(admin): setup basic HTML

--- src/components/admin/admin.component.js
+++ src/components/admin/admin.component.js
@@ -1,10 +1,12 @@
 import template from './admin.html';
 import controller from './admin.controller';
 
+import './admin.styl';
+
 let adminComponent = {
-   template,
-   controller,
-   bindings: {}
+  template,
+  controller,
+  bindings: {}
 };
 
 export default adminComponent;
