f4769b437f8e58321da6b2dc587079f8525584e4
blackendstudios <jeroflat@gmail.com>
2016-08-29 17:55:04 -0500

    refactor(adminUsers): move <admin-users-table> to <admin-users>

--- src/components/admin/admin-dashboard/admin-users/admin-users.js
+++ src/components/admin/admin-dashboard/admin-users/admin-users.js
@@ -3,9 +3,11 @@ import adminUsersComponent from './admin-users.component';
 import AdminUsersService from './admin-users.service';
 
 import adminUsersForm from './admin-users-form/admin-users-form';
+import adminUsersTable from './admin-users-table/admin-users-table';
 
 let adminUsersModule = angular.module('app.admin.dashboard.users', [
-    adminUsersForm.name
+    adminUsersForm.name,
+    adminUsersTable.name
   ])
   .component('adminUsers', adminUsersComponent)
   .service('AdminUsersService', AdminUsersService);
