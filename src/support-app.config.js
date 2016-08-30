831cd62aa8517c64240e42f248241f4e18afeea0
blackendstudios <jeroflat@gmail.com>
2016-08-29 21:59:39 -0500

    feat(support-app.config): setup  configuration

--- src/support-app.config.js
+++ src/support-app.config.js
@@ -1,28 +1,35 @@
 import Firebase from 'firebase';
 
-config.$inject = ['$provide', '$ocLazyLoadProvider', '$stateProvider'];
+config.$inject = ['$provide', '$ocLazyLoadProvider', '$stateProvider', '$firebaseRefProvider'];
 
-function config($provide, $ocLazyLoadProvider, $stateProvider) {
+function config($provide, $ocLazyLoadProvider, $stateProvider, $firebaseRefProvider) {
 
-   $provide.constant('FIRE_REF', Firebase.initializeApp({
-      apiKey: "AIzaSyDwUyJ5sewKQCuIH7I6t6xt4_GXevhyiDU",
-      authDomain: "support-app-e6d70.firebaseapp.com",
-      databaseURL: "https://support-app-e6d70.firebaseio.com",
-      storageBucket: "support-app-e6d70.appspot.com",
-   }));
+  const firebaseConfig = {
+    apiKey: "AIzaSyDwUyJ5sewKQCuIH7I6t6xt4_GXevhyiDU",
+    authDomain: "support-app-e6d70.firebaseapp.com",
+    databaseURL: "https://support-app-e6d70.firebaseio.com",
+    storageBucket: "support-app-e6d70.appspot.com",
+  };
 
-   $stateProvider
-      .state('app', {
-         abstract: true,
-         url: '',
-         template: `
+  $provide.constant('FIRE_REF', Firebase.initializeApp(firebaseConfig));
+
+  $stateProvider
+     .state('app', {
+       abstract: true,
+       url: '',
+       template: `
             <navigation></navigation>
             <support-app></support-app>`
-      });
+     });
 
-   $ocLazyLoadProvider.config({
-      debug: false
-   });
+  $ocLazyLoadProvider.config({
+    debug: false
+  });
+
+  $firebaseRefProvider.registerUrl({
+    default: firebaseConfig.databaseURL,
+    faqs: `${firebaseConfig.databaseURL}/admin/faqs`
+  });
 
 }
 
