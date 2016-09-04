8e1e2fab667358eda3b25e816cac1e2f7ddc3519
blackendstudios <jeroflat@gmail.com>
2016-09-04 00:19:54 -0500

    refactor(adminFaqsTree): move all logic to AdminFaqsService

--- src/components/admin/admin-dashboard/admin-faqs/admin-faqs-tree/admin-faqs-tree.controller.js
+++ src/components/admin/admin-dashboard/admin-faqs/admin-faqs-tree/admin-faqs-tree.controller.js
@@ -9,20 +9,15 @@ class AdminFaqsTreeController {
   }
 
   addNode({item}) {
-    let post = item.nodes.length + 1,
-       newName = `${item.name}-${post}`;
-
-    item.nodes = [...item.nodes, {name: newName, nodes: []}];
-
+    this.AdminFaqsTreeService.addNode(item);
   }
 
   deleteNode({item}) {
-    item.nodes = [];
+    this.AdminFaqsTreeService.deleteNode(item);
   }
 
   deleteCurrentNode(data) {
-    console.log('data');
-    console.dir(data);
+
   }
 
 }
--- src/components/admin/admin-dashboard/admin-faqs/admin-faqs-tree/admin-faqs-tree.service.js
+++ src/components/admin/admin-dashboard/admin-faqs/admin-faqs-tree/admin-faqs-tree.service.js
@@ -1,9 +1,20 @@
 class AdminFaqsTreeService {
-   
-   constructor(BaseService) {
-      this.BaseService = BaseService;
-   }
-   
+
+  constructor(BaseService) {
+    this.BaseService = BaseService;
+  }
+
+  addNode(item) {
+    let post = item.nodes.length + 1,
+       newName = `${item.name}-${post}`;
+
+    item.nodes = [...item.nodes, {name: newName, nodes: []}];
+  }
+
+  deleteNode(item) {
+    item.nodes = [];
+  }
+
 }
 
 AdminFaqsTreeService.$inject = ['BaseService'];
