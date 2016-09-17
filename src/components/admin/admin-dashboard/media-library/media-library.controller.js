98a62ef6472d28339d94b5994bd7493228aa989d
blackendstudios <jeroflat@gmail.com>
2016-09-17 11:25:07 -0500

    feat(mediaLibrary): add uploadFiles method

--- src/components/admin/admin-dashboard/media-library/media-library.controller.js
+++ src/components/admin/admin-dashboard/media-library/media-library.controller.js
@@ -1,9 +1,37 @@
 class MediaLibraryController {
-   
-   constructor(MediaLibraryService) {
-      this.MediaLibraryService = MediaLibraryService;
-   }
-   
+
+  constructor(MediaLibraryService) {
+    this.MediaLibraryService = MediaLibraryService;
+  }
+
+  $onInit() {
+    this.fileList = [];
+
+    this.retrieveFiles();
+  }
+
+  $onChanges(changes) {
+
+  }
+
+  retrieveFiles() {
+    this.MediaLibraryService.retrieveFiles().then(files => {
+      this.fileList = files;
+    })
+    .catch(error => {
+
+    });
+  }
+
+  uploadFiles($files) {
+    this.MediaLibraryService.uploadFiles($files).then(files => {
+      this.fileList = files;
+    })
+    .catch(error => {
+
+    });
+  }
+
 }
 
 MediaLibraryController.$inject = ['MediaLibraryService'];
